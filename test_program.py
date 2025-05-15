bdd9ef9d13ca1b581720565279bb2617ef0d5701
Assemlal11 <assemlaa@oregonstate.edu>
2025-05-15 16:17:52 -0400

    Delete test_program.py

--- test_program.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/forecast?location=Newport,OR&date=2025-05-17"
-response = requests.get(url)
-
-print("Weather Forecast:")
-for day in response.json():
-    print(day)
